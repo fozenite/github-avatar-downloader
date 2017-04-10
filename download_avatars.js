d85556bfe70db69651559ae31b0565e937e64ba2
Rohit Moza <moza.rohit@gmail.com>
2017-04-10 23:47:03 +0000

    Assembled the requestURL together

--- download_avatars.js
+++ download_avatars.js
@@ -4,8 +4,13 @@ var request = require('request');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 
+// -- Setting up constants for our getRepoContributors function ---//
+var GITHUB_USER = "fozenite";                                      // Github username
+var GITHUB_TOKEN = "09e760f4dbf619994b498911ef38dfe495c86136";     // GIthub access token
 
-//---- Get Repo Contributors function ----//
+
+
+// -----------Get Repo Contributors function --------- //
 // -> Uses the request library to fetch                //
 //    the list of contributors via HTTPS               //
 //    for a given repo                                 //
@@ -14,13 +19,17 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 //                        cb  - callback function      //
 //                              handles asynch results //
 //                              returned from function //
+// --------------------------------------------------- //
 function getRepoContributors(repoOwner, repoName, cb) {
+  // ---- Concatenate all the data required to make our request URL --- //
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL); // Testing
 
-  // ------------------- //
+
 
 }
 
-  //---- Passing some hardcoded values to our getRepo ---- //
+//---- Passing some hardcoded values to our getRepo ---------- //
 getRepoContributors("jquery", "jquery" , function(err,result) {
   console.log("Errors:", err);
   console.log("Result" , result);
