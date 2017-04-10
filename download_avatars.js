b0d616b8924b9773756415995b91bb8ef1a35165
Rohit Moza <moza.rohit@gmail.com>
2017-04-10 23:31:55 +0000

    Added the getRepoContributors function

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,27 @@
-// Load the request Module
+// ---- Load the request Module ---- //
 var request = require('request');
-// Welcome message to users
+//---- Welcome message to users ----//
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+
+
+//---- Get Repo Contributors function ----//
+// -> Uses the request library to fetch                //
+//    the list of contributors via HTTPS               //
+//    for a given repo                                 //
+//    ---> Inputs : repoOwner - User Name of Rep Owner //
+//                  repoName  - Name of the Repo       //
+//                        cb  - callback function      //
+//                              handles asynch results //
+//                              returned from function //
+function getRepoContributors(repoOwner, repoName, cb) {
+
+  // ------------------- //
+
+}
+
+  //---- Passing some hardcoded values to our getRepo ---- //
+getRepoContributors("jquery", "jquery" , function(err,result) {
+  console.log("Errors:", err);
+  console.log("Result" , result);
+});
